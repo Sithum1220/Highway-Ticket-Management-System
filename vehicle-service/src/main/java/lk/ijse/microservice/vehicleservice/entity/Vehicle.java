a9dcc16668de2b530431cd64845ebc95378467a9
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-06-25 23:29:43 +0530

    VEHICLE-SERVICE

---
+++ vehicle-service/src/main/java/lk/ijse/microservice/vehicleservice/entity/Vehicle.java
@@ -0,0 +1,23 @@
+package lk.ijse.microservice.vehicleservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String licensePlate;
+    private String model;
+    private String ownerName;
+}
